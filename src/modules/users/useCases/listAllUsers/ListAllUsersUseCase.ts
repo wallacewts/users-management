9947e63f247332ba61233f5c1cdf2aa262050999
Matheus Wallace <matheuswts2011@gmail.com>
2021-03-24 20:25:34 -0300

    add list all users controller and use case

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    this.verifyIfUserIsAnAdmin(user_id);
+
+    const users = this.usersRepository.list();
+
+    return users;
+  }
+
+  verifyIfUserIsAnAdmin(user_id: string): void {
+    const findedUser = this.usersRepository.findById(user_id);
+
+    if (!findedUser.admin) {
+      throw new Error("Not authorized");
+    }
   }
 }
 
